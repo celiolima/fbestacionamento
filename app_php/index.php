df7dd076b175854688f844d9d1e03bd631480969
celiolima <ocelio.bernardo@gmail.com>
2026-05-22 21:50:13 -0300

    .

--- app_php/index.php
+++ app_php/index.php
@@ -36,10 +36,10 @@
  * @since	Version 1.0.0
  * @filesource
  */
-/* echo "</pre>";
+echo "</pre>";
 print_r("  voçê esta na index");
 echo "</pre>";
-exit; */
+exit; 
 
 /*
  *---------------------------------------------------------------
